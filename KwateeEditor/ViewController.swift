8f6ab389aafc90d706851bf39d1605dac66afe73
Jeffrey Varner <jdvarner@purdue.edu>
2015-12-18 13:43:55 -0500

    Initial Commit

---
+++ KwateeEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KwateeEditor
+//
+//  Created by Jeffrey Varner on 12/18/15.
+//  Copyright © 2015 Varnerlab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
